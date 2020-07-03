6c7db9c624bd40338c987f1fa264e454e668dd0c
Nwigwe Uzochukwu <uzochukwunwigwe@gmail.com>
2020-07-03 11:22:33 +0100

    Downloaded dependencies

---
+++ client/payment-management-system/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios';
+
+Vue.config.productionTip = false
+
+// Set up the default vue's http client
+Vue.prototype.$http = axios;
+
+// load the token from the localStorage
+const token = localStorage.getItem("token");
+
+// If there is any token then we wil simply append default axios authorization headers
+if(token) {
+  Vue.prototype.$http.defaults.headers.common['Authorization'] = token;
+}
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ client/payment-management-system/src/router/index.js
@@ -0,0 +1,79 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import store from '../store'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/adminLogin',
+    name: 'adminLogin',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AdminLogin.vue'),
+    meta: {
+      requiresGuest: true
+    }
+  },
+  {
+    path: '/adminLogin',
+    name: 'adminLogin',
+    component: () => import(/* webpackChunkName: "about" */ '../views/AdminLogin.vue'),
+    meta: {
+      requiresGuest: true
+    }
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue'),
+    meta: {
+      requiresGuest: true
+    }
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Profile.vue'),
+    meta: {
+      requiresAuth: true
+    }
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if(!store.getters.isLoggedIn) {
+      // Redirect to the Login Page
+      next('/login');
+    } else {
+      next();
+    }
+  } else if (to.matched.some(record => record.meta.requiresGuest)) {
+    if(store.getters.isLoggedIn) {
+      // Redirect to the profile page
+      next('/profile')
+    } else {
+      next();
+    }
+  } else {
+    next()
+  }
+});
+
+export default router;
